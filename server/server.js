021e4190ed4e3c38bd4d034bfc3b5b0b0add8f7d
jarvis <pratyushshah314@gmail.com>
2025-04-29 23:21:29 +0530

    frontend-fix

--- server/server.js
+++ server/server.js
@@ -18,6 +18,4 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
