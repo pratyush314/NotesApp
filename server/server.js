32af36d4ef7cd7f80800635409d604f382046186
jarvis <pratyushshah314@gmail.com>
2025-04-29 22:49:05 +0530

    fix deploy

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const noteRouter = require("./routes/notes");
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
+
 app.use(cors());
 app.use(express.json());
 
