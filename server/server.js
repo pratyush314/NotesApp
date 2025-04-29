e19c3856bca20ebdc51f4888cf8a2f30b6d707c2
jarvis <pratyushshah314@gmail.com>
2025-04-29 22:51:24 +0530

    fix-deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const noteRouter = require("./routes/notes");
